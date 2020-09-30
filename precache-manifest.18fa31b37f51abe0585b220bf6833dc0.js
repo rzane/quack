e79a8c674fedb517d78eb6cea3708b2abe2d8360
rzane <rzane@users.noreply.github.com>
2020-09-30 03:03:56 +0000

    deploy: 50179aa555e185ecee8b3d5287a921fc9b49398b

---
+++ precache-manifest.18fa31b37f51abe0585b220bf6833dc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9263ae43ab5ac2745ef9eb0e7a5483e4",
+    "url": "/quack/index.html"
+  },
+  {
+    "revision": "241d0ef6b4652a98b0b1",
+    "url": "/quack/static/css/main.f57a8ab7.chunk.css"
+  },
+  {
+    "revision": "4919a81910b5cc485da2",
+    "url": "/quack/static/js/2.4cb10ccb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quack/static/js/2.4cb10ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241d0ef6b4652a98b0b1",
+    "url": "/quack/static/js/main.75a037c3.chunk.js"
+  },
+  {
+    "revision": "1af9d3ffd03f12e4d639",
+    "url": "/quack/static/js/runtime-main.0b62d3e1.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/quack/static/media/profile.5a021ab9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5750c07c6fc90b0e315e331353d7118.js"
+  "/quack/precache-manifest.18fa31b37f51abe0585b220bf6833dc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quack/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
